0c1838038c9eed7d0350c867661220b978f741a0
sbatanapoli-blip <sbatanapoli@gmail.com>
2026-04-17 20:05:58 +0100

    Update ad unit IDs for banner and interstitial ads

--- app/src/main/java/com/galiltv/MainActivity.kt
+++ app/src/main/java/com/galiltv/MainActivity.kt
@@ -22,8 +22,8 @@ class MainActivity : AppCompatActivity() {
     private var backPressedTime: Long = 0
     
     private val HTML_URL = "https://sbatanapoli-blip.github.io/galil-tv-web/"
-    private val BANNER_AD_UNIT_ID = "ca-app-pub-3940256099942544/6300978111"
-    private val INTERSTITIAL_AD_UNIT_ID = "ca-app-pub-3940256099942544/1033173712"
+    private val BANNER_AD_UNIT_ID = "ca-app-pub-2734159647347391/8538520168"
+    private val INTERSTITIAL_AD_UNIT_ID = "ca-app-pub-2734159647347391/6674774154"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
