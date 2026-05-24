5abbf76567d05897c43a5bf3130a65099e713f01
sbatanapoli-blip <sbatanapoli@gmail.com>
2026-05-24 15:00:16 +0100

    Add RelativeLayout import to MainActivity

--- app/src/main/java/com/galiltv/MainActivity.kt
+++ app/src/main/java/com/galiltv/MainActivity.kt
@@ -14,6 +14,7 @@ import android.os.Looper
 import android.util.Log
 import android.webkit.*
 import android.widget.FrameLayout
+import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.*
